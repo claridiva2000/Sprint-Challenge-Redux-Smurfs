882dfadb823bb86a69ba0c8393d6a744e13ec76b
claridiva2000 <claridiva2000@gmail.com>
2019-04-20 11:28:20 -0500

    initial commit, questions complete

---
+++ smurfs/src/components/SmurfsList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function SmurfsList() {
+  return (
+    <div>
+      <h1>SmurfsList Works!</h1>
+    </div>
+  )
+}
+
+
+export default SmurfsList;
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,18 +1,22 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
+import {
+  SMURF_START,
+  SMURF_SUCCESS,
+  SMURF_FAILURE,
+  ADD_SMURF
+} from '../actions/index';
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
-   smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
-   error: null
- }
-*/
+//  Your initial/default state for this project could *Although does not have to* look a lot like this
+const initialState = {
+  smurfs: [],
+  fetchingSmurfs: false,
+  addingSmurf: false,
+  updatingSmurf: false,
+  error: null
+  //  deletingSmurf: false
+};
 
 /*
   You'll only need one smurf reducer for this project.
@@ -21,3 +25,29 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+function reducer(state = initialState, action) {
+  console.log('reducer', action);
+  switch (action.type) {
+    case SMURF_START:
+      return {
+        //fill in later
+      };
+
+    case SMURF_SUCCESS:
+      return {
+        //fill in later
+      };
+    case SMURF_FAILURE:
+      return {
+        //fill in later
+      };
+    case ADD_SMURF:
+      return {
+        //fill in later
+      };
+    default:
+      return state;
+  }
+}
+
+export default reducer;
